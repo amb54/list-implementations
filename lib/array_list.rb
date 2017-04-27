7a11f3a4ae326a4df347dab9217c24769f6dffab
amb54 <ambarklund@hotmail.com>
2017-04-27 11:35:25 -0700

    Added code

--- lib/array_list.rb
+++ lib/array_list.rb
@@ -2,26 +2,61 @@
 
 class ArrayList
   def initialize
-    @storage = []
+    @storage = [0,0,0,0,0,0,0,0,0,0] #capacity
+    @size = 0
   end
 
   # Adds _value_ at the end of the list
   def add(value)
+    raise "bad things" if @size == @storage.length
+    @storage[@size] = value
+    @size += 1
   end
 
   # Deletes the _last_ value in the array
   def delete
+    @storage[@size] = 0
+    @size -= 1
   end
 
   def include?(key)
+    @size.times do |i|
+      return true if @storage[i] == key
+    end
+    return false
   end
 
   def size
+    return @size
   end
 
   def max
+    @size.times do |i|
+      if i == 0
+        max = @storage[0]
+      else
+        if @storage[i] > max
+          max = @storage[i]
+        end
+      end
+    end
+  end
+
+  def max
+    raise "bad things" if @size = 0
+    max = @storage[0]
+    @size.times do |i|
+      if @storage[i] > max
+        max = @storage[i]
+      end
+    end
   end
 
   def to_s
+    str = ""
+    @size.length.times do |i|
+      str += "#{@storage[i]}, "
+    end
+    return "[#{[str[0..-3]]}]"
   end
 end
